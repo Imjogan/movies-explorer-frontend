fe7a9333775a773d7a80547b68c79884447269ed
Ivan Minin <wow.mjogan.official@yandex.ru>
2021-07-13 09:50:04 +0300

    создан и адаптирован под разрешения компонент NavTab

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,10 +1,12 @@
 import './Main.css';
 import Promo from '../Promo/Promo';
+import NavTab from '../NavTab/NavTab';
 
 const Main = () => {
   return (
     <section className="main">
       <Promo />
+      <NavTab />
     </section>
   );
 }
---
+++ src/components/NavTab/NavTab.js
@@ -0,0 +1,13 @@
+import './NavTab.css';
+
+const NavTab = () => {
+  return (
+    <nav className="nav-tab">
+      <a href="#about-project" className="nav-tab__item">О проекте</a>
+      <a href="#technologies" className="nav-tab__item">Технологии</a>
+      <a href="#student" className="nav-tab__item">Студент</a>
+    </nav>
+  );
+};
+
+export default NavTab;
