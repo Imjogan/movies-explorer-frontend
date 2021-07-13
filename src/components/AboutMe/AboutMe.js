2897bb0a14c2edc5413a892c096f310616165894
Ivan Minin <wow.mjogan.official@yandex.ru>
2021-07-13 14:19:09 +0300

    создан и адаптирован под разрешения компонент AboutMe

---
+++ src/components/AboutMe/AboutMe.js
@@ -0,0 +1,56 @@
+import './AboutMe.css';
+import HeaderBlock from '../HeaderBlock/HeaderBlock';
+import studentPhoto from '../../images/student.jpg';
+
+const AboutMe = () => {
+  return (
+    <section id="student" className="about-me">
+      <HeaderBlock title={'Студент'} />
+      <section className="about-me__content">
+        <div className="about-me__info">
+          <p className="about-me__name">Иван</p>
+          <p className="about-me__position">Фронтенд-разработчик, 25 лет</p>
+          <p className="about-me__text">
+            Я живу в Санкт-Петербурге, закончил бакалавриат и магистратуру на
+            факультете радиотехники в СПбГЭТУ "ЛЭТИ". Люблю слушать музыку,
+            путешествовать и гулять на природе. С 2017 года работаю в «НИИ
+            телевидения», занимаюсь настройкой и сдачей контрольно-проверочной
+            аппаратуры для бортовых блоков летательных аппаратов, пишу
+            техническую документацию. Еще во время учебы в Яндекс.Практикуме с
+            другими студентами образовали команду и работаем над реальными
+            проектами.
+          </p>
+          <ul className="about-me__links">
+            <li className="about-me__item-link">
+              <a
+                class="about-me__link"
+                target="_blank"
+                href="https://github.com/Imjogan"
+                rel="noreferrer"
+              >
+                GitHub
+              </a>
+            </li>
+            <li className="about-me__item-link">
+              <a
+                class="about-me__link"
+                target="_blank"
+                href="http://linkedin.com/in/mjogan"
+                rel="noreferrer"
+              >
+                LinkedIn
+              </a>
+            </li>
+          </ul>
+        </div>
+        <img
+          className="about-me__img"
+          src={studentPhoto}
+          alt="Фотография студента"
+        />
+      </section>
+    </section>
+  );
+};
+
+export default AboutMe;
--- src/components/NavTab/NavTab.js
+++ src/components/NavTab/NavTab.js
@@ -3,9 +3,15 @@ import './NavTab.css';
 const NavTab = () => {
   return (
     <nav className="nav-tab">
-      <a href="#about-project" className="nav-tab__item">О проекте</a>
-      <a href="#techs" className="nav-tab__item">Технологии</a>
-      <a href="#student" className="nav-tab__item">Студент</a>
+      <a href="#about-project" target="_self" className="nav-tab__item">
+        О проекте
+      </a>
+      <a href="#techs" target="_self" className="nav-tab__item">
+        Технологии
+      </a>
+      <a href="#student" target="_self" className="nav-tab__item">
+        Студент
+      </a>
     </nav>
   );
 };
